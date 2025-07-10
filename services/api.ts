16edeb4ee78fd0ce1956028a75ae2d392a829598
jahid-bd <jahidbd9x@gmail.com>
2025-07-10 22:48:10 +0600

    feat: enhance movie details and profile screens, add dotenv support, and update API integration

--- services/api.ts
+++ services/api.ts
@@ -1,31 +1,53 @@
 export const TMDB_CONFIG = {
-    BASE_URL: 'https://api.themoviedb.org/3',
-    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
-    }
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
 };
 
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${query}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
-export const fetchMovies = async ({query} : {query: string}) => {
-    const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${query}` : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  const response: any = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
 
-    const response: any = await fetch(endpoint, {
+  if (!response.ok) {
+    throw new Error("Failed to fetch movies", response.status);
+  }
+
+  const data = await response.json();
+
+  return data.results;
+};
+
+export const fetchMovieDetails = async (movieId: string) => {
+  try {
+    const response: any = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
         method: "GET",
         headers: TMDB_CONFIG.headers,
-    })
+      },
+    );
 
-    if(!response.ok){
-        throw new Error("Failed to fetch movies", response.status);
+    if (!response.ok) {
+      throw new Error("Failed to fetch movie details", response.status);
     }
 
-    const data = await response.json()
+    const data = await response.json();
 
-    return data.results;
-
-}
-
+    return data;
+  } catch (error) {
+    console.log("Error fetching movie details:", error);
+    throw error;
+  }
+};
 
 // const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
 // const options = {
